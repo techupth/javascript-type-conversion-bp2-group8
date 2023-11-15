315548b74e103e12778035de0faa3723dc124672
fonytas <fonyfonyta@gmail.com>
2023-11-15 12:38:16 +0700

    feat typeConversion:Exercise1 typeConversion

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,6 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
-
+depositAmountFromUser = +depositAmountFromUser
 accountBalance = accountBalance + depositAmountFromUser;
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
